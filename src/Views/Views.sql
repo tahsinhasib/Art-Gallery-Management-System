a5c0c944e1b761ba06e7a30294b9e594e85143c1
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-12-11 01:30:23 +0600

    Add files via upload

---
+++ src/Views/Views.sql
@@ -0,0 +1,11 @@
+--*** Views
+--1. Create a view displaying ex_id, ex_sponsors, ex_date from exhibitions table
+
+CREATE VIEW view1
+AS
+SELECT ex_id, ex_sponsors, ex_date
+FROM exhibitions
+
+
+select *
+from view1
