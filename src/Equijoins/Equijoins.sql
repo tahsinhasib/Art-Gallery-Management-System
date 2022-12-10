0483e21c8c627daab6d53b1b396e85ff67353af7
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-12-11 00:22:34 +0600

    Update Equijoins.sql

--- src/Equijoins/Equijoins.sql
+++ src/Equijoins/Equijoins.sql
@@ -1,12 +1,21 @@
 --*** Equijoin
 
---1. Display the art_name, artist name for each artists.
-
+--1. Display the ex_name, ex_sponsors, ow_name, ow_email of all the owners
 
 1.
-SELECT A.art_name, 
-        B.atst_name, 
-FROM arts_artist A, artists B
-WHERE A.atst_id = B.atst_id 
+select E.ex_name,
+        E.ex_sponsors,
+        O.ow_name,
+        O.ow_email
+FROM exhibitions E, owners O
+where E.ow_id = O.ow_id
 
 
+--2. Display the owner name, number and their staffs name and id
+
+SELECT O.ow_name,
+        O.ow_num,
+        S.st_name,
+        S.st_id
+FROM owners O, staffs S
+where O.ow_id = S.ow_id
